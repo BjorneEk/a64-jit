0719f706e0405463c0325bff5844dac71158b580
Gustaf franzèn <gustaffranzen@icloud.com>
2025-01-05 12:23:45 +0100

    fixed test for bl

--- tests/a64_bl.c
+++ tests/a64_bl.c
@@ -21,22 +21,21 @@ int main(int argc, const char *argv[])
 	//size_t str_p;
 	FILE *out;
 	a64_jit_init(&jit, 10);
+	a64_jit_push(&jit, a64_mov(R2, R30));
+	a64_jit_push(&jit, a64_movi(R0, 68));
+	a64_jit_push(&jit, a64_bl(3));
+	a64_jit_push(&jit, a64_mov(R30, R2));
+	a64_jit_push(&jit, a64_ret());
 	a64_jit_push(&jit, a64_addi(R0, R0, 1));
 	a64_jit_push(&jit, a64_ret());
-	a64_jit_push(&jit, a64_load_store(STORE, PRE, SZX, 0, R30, SP, -8));
-	a64_jit_push(&jit, a64_load_store(STORE, PRE, SZX, 0, R29, SP, -8));
-	a64_jit_push(&jit, a64_movi(R0, 68));
-	a64_jit_push(&jit, a64_bl(-5));
-	a64_jit_push(&jit, a64_load_store(LOAD, POST, SZX, 0, R29, SP, 8));
-	a64_jit_push(&jit, a64_load_store(LOAD, POST, SZX, 0, R30, SP, 8));
-	a64_jit_push(&jit, a64_ret());
+
 	out = fopen("files/bl.bin", "w");
-	fwrite(&jit.code[2], sizeof(a64_t), jit.length-2, out);
+	fwrite(jit.code, sizeof(a64_t), jit.length, out);
 	fclose(out);
 
 	a64_jit_mkexec(&jit);
 
-	function = (u64_t(*)(void))(uintptr_t)(&jit.code[2]);
+	function = (u64_t(*)(void))(uintptr_t)(jit.code);
 	res = function();
 
 	//if (argc > 1 && strcmp(argv[1], "-v"))
